4067667b2122e982bdfd021fc4167281c5ffaebf
treeforever <treeforever7@gmail.com>
2016-09-28 22:00:10 +0000

    added sign up, jQuery needs more work

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,24 +1,24 @@
-$(function() {
-  $.ajax({
-    method: "GET",
-    url: "/api/users"
-  }).done((users) => {
-    for(user of users) {
-      $("<div>").text(user.name).appendTo($("body"));
-    }
-  });
-
-  $('.login').on('submit','.login-form', function(ev) {
-    let login = $(this).serialize();
-    ev.preventDefault();
-    $.ajax({
-      url: '/login',
-      method: 'post',
-      data: login
-    }).done(function(data) {
-    });
-  });
-});
+// $(function() {
+//   $.ajax({
+//     method: "GET",
+//     url: "/api/users"
+//   }).done((users) => {
+//     for(user of users) {
+//       $("<div>").text(user.name).appendTo($("body"));
+//     }
+//   });
+//
+//   $('.login').on('submit','.login-form', function(ev) {
+//     let login = $(this).serialize();
+//     ev.preventDefault();
+//     $.ajax({
+//       url: '/login',
+//       method: 'post',
+//       data: login
+//     }).done(function(data) {
+//     });
+//   });
+// });
 
 /*function checkUser() {
   $.ajax({
@@ -36,3 +36,22 @@ $(function() {
     }
   });
 }*/
+
+$(function() {
+
+    $('#form-signin-heading').click(function(e) {
+		$(".form-signin").delay(100).fadeIn(100);
+ 		$(".form-register").fadeOut(100);
+		$('#form-signup-heading').removeClass('active');
+		$(this).addClass('active');
+		e.preventDefault();
+	});
+	$('#form-signup-heading').click(function(e) {
+		$(".form-signin").delay(100).fadeIn(100);
+ 		$(".register-form").fadeOut(100);
+		$('#form-signin-heading').removeClass('active');
+		$(this).addClass('active');
+		e.preventDefault();
+	});
+
+});
