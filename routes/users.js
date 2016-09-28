ab6e926619f75b2d60b2501fda37170662daab63
treeforever <treeforever7@gmail.com>
2016-09-28 16:20:28 +0000

    added routes for new.ejs and main.ejs

--- routes/users.js
+++ routes/users.js
@@ -3,16 +3,16 @@
 const express = require('express');
 const router  = express.Router();
 
-module.exports = (knex) => {
-
-  router.get("/", (req, res) => {
-    knex
-      .select("*")
-      .from("users")
-      .then((results) => {
-        res.json(results);
-    });
-  });
-
-  return router;
-}
+// module.exports = (knex) => {
+//
+//   router.get("/", (req, res) => {
+//     knex
+//       .select("*")
+//       .from("users")
+//       .then((results) => {
+//         res.json(results);
+//     });
+//   });
+//
+//   return router;
+// }
