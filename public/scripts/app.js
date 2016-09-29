1abb822cceb67d555cf78fdbd49d324c9784c275
treeforever <treeforever7@gmail.com>
2016-09-29 04:03:44 +0000

    completely adopted the login and signup form

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -38,17 +38,17 @@ $(function() {
 
 $(function() {
 
-    $('#form-signin-heading').click(function(e) {
-		$(".form-signin").delay(100).fadeIn(100);
- 		$(".form-register").fadeOut(100);
-		$('#form-signup-heading').removeClass('active');
+    $('#login-form-link').click(function(e) {
+		$("#login-form").delay(100).fadeIn(100);
+ 		$("#register-form").fadeOut(100);
+		$('#register-form-link').removeClass('active');
 		$(this).addClass('active');
 		e.preventDefault();
 	});
-	$('#form-signup-heading').click(function(e) {
-		$(".form-signin").delay(100).fadeIn(100);
- 		$(".register-form").fadeOut(100);
-		$('#form-signin-heading').removeClass('active');
+	$('#register-form-link').click(function(e) {
+		$("#register-form").delay(100).fadeIn(100);
+ 		$("#login-form").fadeOut(100);
+		$('#login-form-link').removeClass('active');
 		$(this).addClass('active');
 		e.preventDefault();
 	});
